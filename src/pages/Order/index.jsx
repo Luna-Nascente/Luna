26877af0692cef7edaa54c5cd16f999a2b94dfc7
Bertruchio <113859308+Bertruchio@users.noreply.github.com>
2023-06-22 05:09:13 +0300

    order select delivery place

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -4,7 +4,9 @@ import axios from 'axios';
 
 function Order() {
     const [orderItems, setOrderItems] = useState([]);
-    const [delivery, setDelivery] = useState(''); //выбор доставки пока не реализован
+    const [deliveryMethod, setDeliveryMethod] = useState('courier');
+    const [pointsOfDelivery, setPointsOfDelivery] = useState([]);
+    const [selectedPointOfDelivery, setSelectedPointOfDelivery] = useState(3);
 
     useEffect(() => {
         const fetchCartItems = async () => {
@@ -31,28 +33,55 @@ function Order() {
           }
         };
         fetchCartItems();
+
+        const fetchPointsOfDelivery = async () => {
+            try {
+              const response = await axios.get('https://localhost:7256/Points_of_delivery');
+              setPointsOfDelivery(response.data);
+            } catch (error) {
+              console.log(error);
+            }
+          };
+          fetchPointsOfDelivery();
     }, []);
 
-    function handleDeliveryChange(event) {
-        setDelivery(event.target.value);
+    function handleDeliveryMethodChange(event) {
+        setDeliveryMethod(event.target.value);
     }
-
-    const total = orderItems.reduce((acc, item) => acc + item.product_price * item.product_count, 0);
-    const deliveryPrice = 400;
-
-    // function showCdekWidget() {
-    //     var widget = document.getElementById('cdek-widget');
-    //     if (widget) {
-    //         widget.style.display = 'block';
-    //     }
-    // }
+    
+      function handlePointOfDeliveryChange(event) {
+        setSelectedPointOfDelivery(event.target.value);
+    }
+    
+        const total = orderItems.reduce((acc, item) => acc + item.product_price * item.product_count, 0);
+        const deliveryPrice = deliveryMethod === 'courier' ? 400 : 0;
+    
+    function handleSubmit(event) {
+        event.preventDefault();
+    
+        const data = {
+          order_date: new Date().toISOString().slice(0, 10),
+          client_id: 1,
+          total_price: total + deliveryPrice,
+          point_of_delivery_address: selectedPointOfDelivery,
+          order_status: 'new'
+        };
+    
+        axios.post('https://localhost:7256/Orders', data)
+          .then(response => {
+            console.log(response.data);
+            // очистить корзину или перенаправить пользователя на страницу подтверждения заказа
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
 
     return(
         <div className={styles.order}>
             <h1>Ordering</h1>
 
-            {/* method: post */}
-            <form action=""> 
+            <form onSubmit={handleSubmit}> 
                 <div class={styles.left_col}>
                     <label for="name">Your name:</label>
                     <input type="text" name="name" id="name" required/>
@@ -62,27 +91,37 @@ function Order() {
 
                     <label for="email">Email:</label>
                     <input type="email" name="email" id="email" required/>
-
-                    <label for="country">Country:</label>
-                    <input type="text" name="country" id="country" required/>
-
-                    <label for="city">Settlement:</label>
-                    <input type="text" name="city" id="city" required/>
                     
                     <label>Select your delivery method:</label>
                     <div className={styles.delivery}>
                         <div className='d-flex'>
-                            <input className='delivery' type="radio" name="delivery" id="courier" value="courier" checked={delivery === "courier"} onChange={handleDeliveryChange} />
-                            <label className="delivery" htmlFor="courier"><p>By courier ({deliveryPrice} rub.)</p></label>
+                        <input className='delivery' type="radio" name="delivery" id="courier" value="courier" checked={deliveryMethod === "courier"} onChange={handleDeliveryMethodChange} />
+                        <label className="delivery" htmlFor="courier"><p>By courier ({deliveryPrice} rub.)</p></label>
                         </div>
                         <div className='d-flex'>
-                            <input type="radio" name="delivery" id="sdek" value="sdek" checked={delivery === "sdek"} onChange={handleDeliveryChange} />
-                            <label htmlFor="sdek"><p>To the SDEK pickup point</p></label>
-                            {/* <label htmlFor="sdek"><a href="#" onClick={showCdekWidget}>В пункт выдачи СДЭК</a></label>
-                            <div id="cdek-widget" data-type="PVZ" data-inpost-id="id_вашего_виджета" style={{ display: 'none' }}/>
-                            <script type="text/javascript" src="//widget.cdek.ru/widget/widjet.js"></script> */}
-                        </div>  
+                        <input type="radio" name="delivery" id="sdek" value="sdek" checked={deliveryMethod === "sdek"} onChange={handleDeliveryMethodChange} />
+                        <label htmlFor="sdek"><p>To the pickup point</p></label>
+                        </div>
                     </div>
+
+                    {deliveryMethod === 'courier' ? (
+                        <div className={styles.delivery_address}>
+                        <label htmlFor="delivery_address">Delivery address:</label>
+                        <input type="text" name="delivery_address" id="delivery_address" required />
+                        </div>
+                    ) : (
+                        <div className={styles.point_of_delivery}>
+                        <label htmlFor="point_of_delivery">Select a pickup point:</label>
+                        <select name="point_of_delivery" id="point_of_delivery" value={selectedPointOfDelivery} onChange={handlePointOfDeliveryChange} required>
+                            <option value="">-- Select a pickup point --</option>
+                            {pointsOfDelivery.map(point => (
+                                <option key={point.point_of_delivery_id} 
+                                value={point.point_of_delivery_address}>
+                                    {point.point_of_delivery_address}</option>
+                            ))}
+                        </select>
+                        </div>
+                    )}
                     
                     <label for="birthdate">Enter your date of birth:</label>
                     <input type="date" name="birthdate" id="birthdate"/>
